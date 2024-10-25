41e478d60164d02e1448ca96069b0ab3b077e87a
Rafa <rafael.rued4@gmail.com>
2024-10-25 02:31:06 -0300

    git ignore pt2

---
+++ Editora/editora/Registravel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace editora
+{
+    internal class Registravel {
+        public Registravel() { }
+
+        public static void SalvarCSV<T> (List<T> dados, string caminho, Func<T, string> salvar) {
+            try {
+                using (StreamWriter writer = new StreamWriter(caminho)) {
+                    foreach (T dado in dados) {
+                        writer.WriteLine(salvar(dado));
+                    }
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
